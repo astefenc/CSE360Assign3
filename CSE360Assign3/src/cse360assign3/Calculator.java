39ce008288f4e07169622c319d842a5bf33b2c75
Alyssa <astefenc@asu.edu>
2019-10-21 21:36:00 -0700

    finished

--- CSE360Assign3/src/cse360assign3/Calculator.java
+++ CSE360Assign3/src/cse360assign3/Calculator.java
@@ -10,37 +10,37 @@ package cse360assign3;
 import java.lang.String;
 
 
-public class Calculator extends AddingMachine {
+public class Calculator extends AddingMachine { //use extend her inheritance 
 	
 	//protected int newTotal; //new total for calculator
 	
-	public Calculator()
+	public Calculator() //set total to zero
 	{
 		total = 0;
 	}
-	public void divide(int value) {
+	public void divide(int value) { 
 		if(value == 0)
 		{
-			total = 0;
-			output = output + " / 0 ";
+			total = 0; //if zero make total zero
+			output = output + " / 0 "; //add to output
 		}
 		else
 		{
-			total = total / value;
-			output = output + " / " + value;
+			total = total / value; //if not zero divide
+			output = output + " / " + value; //add to output
  		}
 	}
 	
 	public void multiply(int value) {
-		if(value == 0)
+		if(value == 0) 
 		{
-			total = 0;
-			output = output + " * 0 ";
+			total = 0; //if zero set total to zero
+			output = output + " * 0 "; //add to output
 		}
 		else
 		{
-			total = total * value;
-			output = output + " * " + value;
+			total = total * value; //if not zero multiply
+			output = output + " * " + value; //add to output
 		}
 	}
 	
